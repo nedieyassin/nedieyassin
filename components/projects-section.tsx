c40671c5c453f5c491ff21bb630b8399af656e87
Yassin Nedie <yassinnedie9121@gmail.com>
2024-06-03 15:57:17 +0200

    Build

--- components/projects-section.tsx
+++ components/projects-section.tsx
@@ -122,12 +122,12 @@ export default function ProjectsSection() {
                 <h1 className="text-5xl text-slate-800">Projects</h1>
                 <div className="grid gap-12">
                     {
-                        projects.filter((item, index) => showMore || index < 5). map((item, index) => (
+                        projects.filter((item, index) => showMore || index < 5).map((item, index) => (
                             <div key={index} className="space-y-2">
                                 <h1 className="font-bold text-xl text-slate-800">
                                     {item.title}
                                 </h1>
-                                <h2 className="font-normal text-sm">{item.year} { item.at ? `- at ${item.at}` : '' }</h2>
+                                <h2 className="font-normal text-sm">{item.year} {item.at ? `- at ${item.at}` : ''}</h2>
                                 <p className="text-slate-800 text-lg">
                                     {item.description}
                                 </p>
@@ -137,7 +137,8 @@ export default function ProjectsSection() {
                                     ))}
                                 </ul>
                                 {item.url && (
-                                    <a href={item.url} target="_blank" className="inline-block font-normal text-primary-900 underline">
+                                    <a href={item.url} target="_blank"
+                                       className="inline-block font-normal text-primary-900 underline">
                                         Visit project
                                     </a>
                                 )}
@@ -146,7 +147,7 @@ export default function ProjectsSection() {
                     }
                     <div>
                         <button onClick={() => setShowMore((v) => !v)}
-                                className="border-2 border-primary-500 text-primary-500 rounded px-6 py-2">
+                                className="border-2 border-primary-500 text-primary-900 hover:bg-primary-100 transition-all rounded px-6 py-2">
                             View {showMore ? 'less' : 'all'}
                         </button>
                     </div>
